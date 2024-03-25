269a260c3c79d578f19705a12599d2232837831e
nevinnoby <nevinmadappallil@gmail.com>
2024-03-25 20:20:45 +0530

    addevnet to db

--- backend/routes/addevent/oraganization.js
+++ backend/routes/addevent/oraganization.js
@@ -1,9 +1,18 @@
 const express=require("express");
 var router = express.Router();
 const bodyParser=require("body-parser");
-const {addevent} = require('../../database/db')
-
+const {addevent} = require('../../database/db');
+const mongoose=require('mongoose');
 const app=express();
+
+
+const newEvent={
+    name:"event5",
+    location:"kottym",
+    date:"2020-10-02"
+
+};
+addevent.insertMany([newEvent])
 
 
 app.use(bodyParser.json());
@@ -11,17 +20,13 @@ app.use(bodyParser.json());
 // let events=[];
 
 app.post('/',(req,res)=>{
-    const newevent = new addevent(req.body);
-
-    
-
+    const {eventName,date,location}=req.body;
+    addevent.insertMany([newEvent])
+    app.use(bodyParser.json());
     // events.push(newEvent);
     // console.log(new_event);
     res.status(201).json({message:'Event registered succesfully',event:newevent});
 });
 
-// const PORT=3000;
-// app.listen(PORT,()=>{
-//     console.log(`server is running on port ${PORT}`);
-// });
+
 module.exports=router;

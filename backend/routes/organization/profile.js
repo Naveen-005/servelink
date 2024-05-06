f0eaf16d6daf62d16e8e132d805b4d19b42ec15c
Naveen-005 <naveenalex512@gmail.com>
2024-05-06 20:50:03 +0530

    major changes

--- backend/routes/organization/profile.js
+++ backend/routes/organization/profile.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
     if(req.query.id){
 
     organizationModel.findById(req.query.id).select('-password').then((m_res)=>{
-        console.log(m_res)
+        //console.log(m_res)
         res.send(m_res)
     })
     //console.log(req.query.id)

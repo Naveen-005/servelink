1ad771d02c71da9d86fab799e8650a12f5d8b2d2
Naveen-005 <naveenalex512@gmail.com>
2024-06-20 10:57:35 +0530

    badge imges added

--- backend/routes/achievments.js
+++ backend/routes/achievments.js
@@ -10,23 +10,26 @@ const upload = multer({ storage: storage });
 
 
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
 
+
+
+  res.send('respond with a resource');
 
 });
 
 router.post('/',upload.single('file'), function(req, res, next) {
-    res.send('respond with a resource');
+    
 
-    //textOverlay();
+  //textOverlay(text);
+  res.send('respond with a resource');
 });
 
 
-async function textOverlay(){
+async function textOverlay(text){
     //const image=await Jimp.read('backend/Untitled.png');
     const font=await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
-    image.print(font,5,5,"Hero Badge");
-    console.log(image)
+    image.print(font,5,5,text);
+    //console.log(image)
     //await image.writeAsync('edited.png');
 }
 

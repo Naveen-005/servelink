a6814e68bdc504ed6d9c2159d6b7b7eeddac7562
Naveen-005 <naveenalex512@gmail.com>
2024-07-04 13:13:25 +0530

    badges displayed

--- backend/routes/achievments.js
+++ backend/routes/achievments.js
@@ -1,26 +1,49 @@
 var express = require('express');
 var router = express.Router();
-const { achievmentModel } = require('../database/db')
+const { achievmentModel,eventModel,organizationModel } = require('../database/db')
 let Jimp = require('jimp');
 const { minioClient } = require('../database/bucket_storage')
 const multer = require('multer');
 const storage = multer.memoryStorage(); // Store file data in memory
 const upload = multer({ storage: storage });
 
+router.get('/', async function(req, res, next) {
+  try {
+    const achievments = await achievmentModel.find({ vol_id: req.query.id });
 
+  
+    const eventIds = achievments.map(achievment => achievment.event_id);
 
-router.get('/', function(req, res, next) {
+    const orgIds = achievments.map(achievment => achievment.org_id);
 
+    const events = await eventModel.find({ _id: { $in: eventIds } }, 'title');
+  
+    const organizations = await organizationModel.find({ _id: { $in: orgIds } }, 'name');
 
+    
 
-  res.send('respond with a resource');
-
+ 
+    const formattedAchievments = achievments.map(achievment => ({
+      _id: achievment._id,
+      vol_id: achievment.vol_id,
+      event_title: events.find(event => event._id.toString() === achievment.event_id)?.title || "Unknown Event",
+      title: achievment.title,
+      description: achievment.description,
+      org_name: organizations.find(org => org._id.toString() === achievment.org_id)?.name || "Unknown Organization",
+    }));
+    console.log(formattedAchievments)
+    res.send(formattedAchievments);
+  } catch (err) {
+    console.error("Error fetching achievments:", err);
+    res.status(500).send("Error fetching achievments");
+  }
 });
+
+
 
 router.post('/',upload.single('file'), function(req, res, next) {
     
 
-  //textOverlay(text);
   req.body.form.org_id=req.cookies.org_id
 
   const achievment_instance = new achievmentModel(req.body.form);
@@ -43,12 +66,5 @@ router.post('/',upload.single('file'), function(req, res, next) {
 });
 
 
-async function textOverlay(text){
-    //const image=await Jimp.read('backend/Untitled.png');
-    const font=await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
-    image.print(font,5,5,text);
-    //console.log(image)
-    //await image.writeAsync('edited.png');
-}
 
 module.exports = router;
